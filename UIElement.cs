09f624cfafb8bfe175ce3f6eaee77ea2a73a5e4c
Jonah Hansen <43560715+Jonah-Hansen@users.noreply.github.com>
2024-02-16 22:28:51 -0600

    Update UIElement.cs

--- UIElement.cs
+++ UIElement.cs
@@ -6,12 +6,11 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(CanvasGroup), typeof(RectTransform))]
 public abstract class UIElement : MonoBehaviour
 {
+    [field: SerializeField] public GameObject SelectedObject { get; set; }
     RectTransform _rect;
     public RectTransform RectTransform => _rect;
     CanvasGroup _canvasGroup;
-    public CanvasGroup CanvasGroup => _canvasGroup;
-    [field: SerializeField] public GameObject SelectedObject { get; set; }
-
+    protected CanvasGroup CanvasGroup => _canvasGroup;
     public event Action CloseRequestEvent;
     public event Action CloseEvent;
 
@@ -19,12 +18,12 @@ public abstract class UIElement : MonoBehaviour
         WaitOneFrame(() => gameObject.SetActive(false));
     }
     
-    public virtual void Awake() {
+    protected virtual void Awake() {
         _rect = GetComponent<RectTransform>();
         _canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    public virtual void OnCloseButton() => CloseRequestEvent?.Invoke();
+    protected virtual void OnCloseButton() => CloseRequestEvent?.Invoke();
 
     public virtual void Show() {
         gameObject.SetActive(true);
@@ -38,13 +37,21 @@ public abstract class UIElement : MonoBehaviour
     }
 
     /// <summary> in case if overriding Hide, need to manually Raise CloseEvent </summary>
-    public void RaiseCloseEvent() => CloseEvent?.Invoke();
+    protected void RaiseCloseEvent() => CloseEvent?.Invoke();
 
-    public void WaitOneFrame(Action callback) {
+    protected void WaitOneFrame(Action callback) {
         StartCoroutine(Coroutine(callback));
         static IEnumerator Coroutine(Action callback) {
             yield return null;
             callback();
         }
+    }
+
+    public abstract void SetButtonIcons(UIButtonIconSet icons);
+
+    [Serializable]
+    public class UIButtonIconSet {
+        [field:SerializeField] public string Confirm { get; set; }
+        [field:SerializeField] public string Cancel { get; set; }
     }
 }
