ac8a165b7699fe7dde888b7e7ff55d4f5a2a380a
Jonah Hansen <43560715+Jonah-Hansen@users.noreply.github.com>
2024-03-25 16:21:47 -0600

    Update UIElement.cs

--- UIElement.cs
+++ UIElement.cs
@@ -7,23 +7,30 @@ using UnityEngine.EventSystems;
 public abstract class UIElement : MonoBehaviour
 {
     [field: SerializeField] public GameObject SelectedObject { get; set; }
-    RectTransform _rect;
+    
+    private RectTransform _rect;
     public RectTransform RectTransform => _rect;
-    CanvasGroup _canvasGroup;
+
+    private CanvasGroup _canvasGroup;
     protected CanvasGroup CanvasGroup => _canvasGroup;
+
     public event Action CloseRequestEvent;
     public event Action CloseEvent;
 
+    public abstract void SetButtonIcons(UIButtonIconSet icons);
+
     public virtual void Initialize() {
+        gameObject.SetActive(true);
+        // gives awake a chance to run Awake() before the page is hidden, and for layouts to arrange themselves.
         WaitOneFrame(() => gameObject.SetActive(false));
     }
-    
-    protected virtual void Awake() {
-        _rect = GetComponent<RectTransform>();
-        _canvasGroup = GetComponent<CanvasGroup>();
+    public virtual void OnCloseButton() {
+        CloseRequestEvent?.Invoke();
+        AudioManager audio = AudioManager.Instance;
+        if(audio) {
+            audio.Play("sfx-ui-buttonconf");
+        }
     }
-
-    protected virtual void OnCloseButton() => CloseRequestEvent?.Invoke();
 
     public virtual void Show() {
         gameObject.SetActive(true);
@@ -36,8 +43,15 @@ public abstract class UIElement : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(null);
     }
 
-    /// <summary> in case if overriding Hide, need to manually Raise CloseEvent </summary>
-    protected void RaiseCloseEvent() => CloseEvent?.Invoke();
+      protected virtual void Awake() {
+        _rect = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    /// <summary> in case of overriding Hide, need to manually Raise CloseEvent </summary>
+    protected void RaiseCloseEvent() {
+        CloseEvent?.Invoke();
+    }
 
     protected void WaitOneFrame(Action callback) {
         StartCoroutine(Coroutine(callback));
@@ -46,8 +60,6 @@ public abstract class UIElement : MonoBehaviour
             callback();
         }
     }
-
-    public abstract void SetButtonIcons(UIButtonIconSet icons);
 
     [Serializable]
     public class UIButtonIconSet {
