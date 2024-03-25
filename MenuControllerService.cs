d1fb9e10298ccb7ce75c84332922a441eaaa8950
Jonah Hansen <43560715+Jonah-Hansen@users.noreply.github.com>
2024-03-25 16:18:12 -0600

    Update MenuControllerService.cs

--- MenuControllerService.cs
+++ MenuControllerService.cs
@@ -4,74 +4,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-
-public abstract class MenuControllerService : ScriptableObject
+    
+public abstract class MenuController : MonoBehaviour
 {
-    [SerializeField] PlayerControlsInputService _inputService;
-    PlayerControlsInputService InputService => _inputService;
+    [SerializeField] private PlayerControlsInputService _inputService;
+    private PlayerControlsInputService InputService => _inputService;
 
-    [Header("Element 0 should be the initial page for this menu")]
-    [SerializeField] GameObject[] _pagePrefabs;
-    [SerializeField] ControlSchemeButtonIcons[] _controlSchemeIcons;
-    ControlSchemeButtonIcons[] ControlSchemeIcons => _controlSchemeIcons;
-    UIElement[] Pages { get; set; } 
-    Stack<UIElement> Stack { get; } = new();
+    [SerializeField] private ControlSchemeButtonIcons[] _controlSchemeIcons;
+    private ControlSchemeButtonIcons[] ControlSchemeIcons => _controlSchemeIcons;
+
+    [SerializeField] private UIElement[] _pages;
+    private UIElement[] Pages => _pages;
+    
+    private Stack<UIElement> Stack { get; } = new();
 
     public event Action MenuClosedEvent;
     public event Action MenuOpenedEvent;
 
-    public virtual void Initialize(Canvas rootCanvas) {
+    protected virtual void Start() {
         Stack.Clear();
-        List<UIElement> pagesList = new();
-        foreach(GameObject pagePrefab in _pagePrefabs) {
-            GameObject pageObject = Instantiate(pagePrefab, rootCanvas.transform);
-            if(pageObject.TryGetComponent(out UIElement page)) {
-                pagesList.Add(page);
-                page.Initialize();
-            } else {
-                Destroy(pageObject);
-                Debug.LogError($"Menu Controller {this} tried to initialize {pageObject} which is not a UIElement.");
-            }
+        foreach(UIElement page in Pages) {
+            page.Initialize();
         }
-        Pages = pagesList.ToArray();
     }
 
     protected abstract void SubscribeEvents();
     protected abstract void UnsubscribeEvents();
 
-    void SubscribeInputEvents() {
-        InputService.UICancelEvent += Back;
-        InputService.ControlSchemeChangedEvent += OnControlSchemeChange;
-    }
-    void UnsubscribeInputEvents() {
-        InputService.UICancelEvent -= Back;
-        InputService.ControlSchemeChangedEvent -= OnControlSchemeChange;
+    /// <summary> opens the first page in the menu </summary>
+    public void Open() {
+        if(Stack.TryPeek(out UIElement top)) {
+            return;
+        }
+        Stack.Push(Pages[0]);
+        Pages[0].Show();
+        OpenMenu();
     }
 
     protected T GetPage<T>() where T : UIElement {
         for(int i = 0; i < Pages.Length; i++) {
-            if(Pages[i] is T page) return page;
+            if(Pages[i] is T page) {
+                return page;
+            }
         } 
         Debug.LogError($"no page of type {typeof(T)} found");
         return null;
     }
-
-    /// <summary> opens the first page in the menu </summary>
-    public void Open() {
-        if(Stack.TryPeek(out UIElement top)) return;
-        InputService.CurrentActionMap = InputService.Controls.UI;
-        Stack.Push(Pages[0]);
-        Pages[0].Show();
-        MenuOpenedEvent?.Invoke();
-        SubscribeInputEvents();
-        OnControlSchemeChange(InputService.CurrentControlScheme);
-        SubscribeEvents();
+    
+    protected bool TryGetPage<T>(out T page) where T : UIElement {
+        page = null;
+        for(int i = 0; i < Pages.Length; i++) {
+            if(Pages[i] is T found) {
+                page = found;
+                return true;
+            }
+        } 
+        return false;
     }
 
-    public void Open(UIElement page, bool hidePrev = false) {
-        if(page.gameObject.activeInHierarchy) return;
-        if(hidePrev) {
-            Stack.Peek().Hide();
+    protected void Open(UIElement page, bool hidePrev = false) {
+        if(page.gameObject.activeInHierarchy) {
+            return;
+        }
+        if(Stack.TryPeek(out UIElement top)) {
+            if(hidePrev) {
+                top.Hide();
+            }
+        } else {
+            OpenMenu();
         }
         Stack.Push(page);
         page.Show();
@@ -81,24 +81,43 @@ public abstract class MenuControllerService : ScriptableObject
     protected void Close() {
         while(Stack.Count > 0) Back(true);
         MenuClosedEvent?.Invoke();
-        UnsubscribeInputEvents();
-        UnsubscribeEvents();
     }
 
     protected void Back() {
         Back(false);
     }
-    void Back(bool force) {
+
+    private void OpenMenu() {
+        InputService.CurrentActionMap = InputService.Controls.UI;
+        MenuOpenedEvent?.Invoke();
+        OnControlSchemeChange(InputService.CurrentControlScheme);
+    }
+
+    private void OnEnable() {
+        SubscribeEvents();
+        InputService.UICancelEvent += Back;
+        InputService.ControlSchemeChangedEvent += OnControlSchemeChange;
+    }
+
+    private void OnDisable() {
+    UnsubscribeEvents();
+        InputService.UICancelEvent -= Back;
+        InputService.ControlSchemeChangedEvent -= OnControlSchemeChange;
+    }
+
+    private void Back(bool force) {
         Stack.Pop().Hide(force);
         if(!Stack.TryPeek(out UIElement prev)) {
             Close();
             return;
         }
         EventSystem.current.SetSelectedGameObject(prev.SelectedObject);
-        if(!prev.gameObject.activeInHierarchy) prev.Show();
+        if(!prev.gameObject.activeInHierarchy) {
+            prev.Show();
+        }
     }
 
-    void OnControlSchemeChange(InputControlScheme scheme) {
+    private void OnControlSchemeChange(InputControlScheme scheme) {
         ControlSchemeButtonIcons icons = Array.Find(ControlSchemeIcons, (icons) => icons.Scheme == scheme.name);
         if(icons == null) {
             Debug.LogWarning($"no icons found for scheme {scheme.name}");
@@ -110,7 +129,7 @@ public abstract class MenuControllerService : ScriptableObject
     }
 
     [Serializable]
-    class ControlSchemeButtonIcons {
+    private class ControlSchemeButtonIcons {
         [Header("must match a Scheme name in the Input Actions Asset")]
         [SerializeField] string _scheme;
         public string Scheme => _scheme;
